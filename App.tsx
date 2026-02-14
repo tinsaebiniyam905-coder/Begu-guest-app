94967eec7a2c419c617173bcc82d5c53aadb30f4
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-02-14 05:44:36 -0800

    Fix import case for Layout component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import Layout from './components/Layout.tsx';
+import Layout from './components/layout';
 import ReceptionView from './components/ReceptionView';
 import PoliceView from './components/PoliceView';
 import SettingsView from './components/SettingsView';
